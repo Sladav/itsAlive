589181b3bcfedf7a8a2d705da8bddcf9b62813f0
Steve Ladavich <steve.ladavich@gmail.com>
2016-12-19 23:06:34 -0500

    Added all basic features

--- src/index.js
+++ src/index.js
@@ -1,32 +1,81 @@
 
 function valueOf() {
-  return this.value
+  return this._value
 }
 
-function update() {}
+function update(value) {
+  let newValue
+
+  // if frozen -> don't update
+  if(this._isFrozen) return this
+
+  // if explicit update -> don't use reducer/inputs
+  // else if -> set new value to undefined
+  // else -> use reducer/inputs to update
+  if(typeof value !== 'undefined') {
+    newValue = value
+  } else if(typeof this._reducer !== 'function') {
+    newValue = undefined
+  } else {
+    newValue = this._reducer(...this._inputs.map(input=>input.valueOf()))
+  }
+
+  // if new value is undefined -> don't update, don't notify
+  //  note: new value is undefined when (given no explicit value)...
+  //    (1) reducer is not a function
+  //    (2) reducer returned an undefined value
+  if(typeof newValue === 'undefined') return this
+  this._value = newValue
+
+  // notify
+  //  if not quiet -> notify
+  //  that is: if quiet -> don't notify
+  if(!this._isQuiet) this._listeners.forEach(listener=>listener.update())
+
+  return this
+}
 
 function setReducer(reducerFn) {
-
+  if(arguments.length > 1) throw new Error('expected single argument')
+  if(typeof reducerFn !== 'function') throw new TypeError('expected a function')
+  this._reducer = reducerFn
+  return this
 }
 
-function setInput() {}
+function setInput(...inputs) {
+  this._inputs = inputs
+  return this
+}
 
-function listenTo() {}
+function listenTo(...liveValues) {
+  liveValues.forEach((liveValue) => {
+    if(!liveValue.hasOwnProperty('_listeners') || !Array.isArray(liveValue._listeners)) {
+      throw new TypeError('Expected a live value')
+    }
+    liveValue._listeners.push(this)
+    return liveValue
+  })
+  return this
+}
 
 function freeze() {
-  this.isFrozen = true
+  this._isFrozen = true
+  return this
 }
 
 function unfreeze() {
-  this.isFrozen = false
+  this._isFrozen = false
+  return this
 }
 
 function quiet() {
-  this.isQuiet = true
+  this._isQuiet = true
+  return this
 }
 
 function unquiet() {
-  this.isQuiet = false
+  this._isQuiet = false
+  return this
 }
 
 const prototype = {
@@ -34,6 +83,7 @@ const prototype = {
   update,
   setReducer,
   setInput,
+  setInputs: setInput,
   listenTo,
   freeze,
   unfreeze,
@@ -43,12 +93,12 @@ const prototype = {
 
 function itsAlive(initialValue = null) {
   return Object.assign(Object.create(prototype), {
-    value: initialValue,
-    reducer: null,
-    inputs: [],
-    listeners: [],
-    isFrozen: false,
-    isQuiet: false
+    _value: initialValue,
+    _reducer: null,
+    _inputs: [],
+    _listeners: [],
+    _isFrozen: false,
+    _isQuiet: false
   })
 }
 
--- test/itsAlive.spec.js
+++ test/itsAlive.spec.js
@@ -1,6 +1,10 @@
 import itsAlive from '../src'
 import {expect} from 'chai'
 
+function untested(){
+  return expect('tested').to.be.true
+}
+
 //clear test console
 console.log('\x1Bc')
 
@@ -13,12 +17,12 @@ describe('itsAlive', () => {
   it('returns an "alive" value', () => {
     // duck type to determine if living value
     const properties = [
-      'value',
-      'reducer',
-      'inputs',
-      'listeners',
-      'isFrozen',
-      'isQuiet'
+      '_value',
+      '_reducer',
+      '_inputs',
+      '_listeners',
+      '_isFrozen',
+      '_isQuiet'
     ]
 
     const methods = [
@@ -60,17 +64,417 @@ describe('living value', () => {
   it('has the correct default values', () => {
     const value = itsAlive()
 
-    expect(value.value).to.equal(null)
+    expect(value._value).to.equal(null)
     expect(value.valueOf()).to.equal(null)
 
-    expect(value.reducer).to.equal(null)
-    expect(value.inputs).to.deep.equal([])
-    expect(value.listeners).to.deep.equal([])
-    expect(value.isFrozen).to.equal(false)
-    expect(value.isQuiet).to.equal(false)
-    
+    expect(value._reducer).to.equal(null)
+    expect(value._inputs).to.deep.equal([])
+    expect(value._listeners).to.deep.equal([])
+    expect(value._isFrozen).to.equal(false)
+    expect(value._isQuiet).to.equal(false)
+
   })
 
+})
 
+describe('setting the reducer', () => {
+
+  it('only accepts a single function', () => {
+    const value = itsAlive()
+
+    expect(()=>value.setReducer()).to.throw()
+    expect(()=>value.setReducer(0)).to.throw()
+    expect(()=>value.setReducer(false)).to.throw()
+    expect(()=>value.setReducer('test')).to.throw()
+    expect(()=>value.setReducer([])).to.throw()
+    expect(()=>value.setReducer({})).to.throw()
+
+    expect(()=>value.setReducer(()=>{})).to.not.throw()
+
+    expect(()=>value.setReducer(()=>{},()=>{})).to.throw()
+  })
+
+  it('exposes the reducer on the "_reducer" property', () => {
+    const value = itsAlive()
+    const identity = x => x
+    value.setReducer(identity)
+
+    expect(value._reducer).to.equal(identity)
+  })
+
+  it('allows method chaining', () => {
+    const value = itsAlive()
+    const identity = x => x
+
+    expect(value.setReducer(identity)).to.equal(value)
+  })
+
+})
+
+describe('setting the inputs', () => {
+
+  it('stores variadic arguments to "_inputs" property as an array', () => {
+    const value = itsAlive()
+    let a=1, b={a:1}, c=[1,2,3], d=()=>{return true}
+    value.setInput(a, b, c, d)
+
+    expect(value._inputs).to.be.deep.equal([a,b,c,d])
+  })
+
+  it('allows method chaining', () => {
+    const value = itsAlive()
+    let a=1, b={a:1}, c=[1,2,3], d=()=>{return true}
+    value.setInput(a, b, c, d)
+
+    expect(value.setInput(a, b, c, d)).to.be.equal(value)
+  })
+
+})
+
+describe('listening to other live values', () => {
+
+  it('throws error if other is not listenable', () => {
+    const a = itsAlive('a')
+    const b = itsAlive('b')
+
+    expect(()=>a.listenTo(0)).to.throw()
+    expect(()=>a.listenTo(false)).to.throw()
+    expect(()=>a.listenTo('test')).to.throw()
+    expect(()=>a.listenTo({})).to.throw()
+    expect(()=>a.listenTo([])).to.throw()
+    expect(()=>a.listenTo(()=>{})).to.throw()
+
+    expect(()=>a.listenTo(b)).to.not.throw()
+    expect(()=>a.listenTo({_listeners:[]})).to.not.throw()
+  })
+
+  it('adds listeners to the "listeners" array property', () => {
+    const a = itsAlive('a')
+    const b = itsAlive('b')
+    const c = itsAlive('c')
+    a.listenTo(b,c)
+
+    expect(b._listeners).to.include(a)
+    expect(c._listeners).to.include(a)
+  })
+
+  it('allows method chaining', () => {
+    const a = itsAlive('a')
+    const b = itsAlive('b')
+    const c = itsAlive('c')
+    a.listenTo(b,c)
+
+    expect(a.listenTo(b,c)).to.be.equal(a)
+  })
+
+})
+
+describe('updating a value', () => {
+
+  describe('with supplied value', () => {
+
+    it('updates to supplied value', () => {
+      const value = itsAlive(0)
+      value.update(1)
+
+      expect(value.valueOf()).to.equal(1)
+    })
+
+    it('notifies listeners', () => {
+      const value = itsAlive(0)
+      const updated = itsAlive(false)
+        .listenTo(value)
+        .setReducer(()=>true)
+
+      value.update(1)
+
+      expect(updated.valueOf()).to.true
+    })
+
+  })
+
+  describe('without supplied value', () => {
+
+    describe('with proper reducer', () => {
+
+      describe('when reducer result is defined', () => {
+
+        it('updates to reduced input values', () => {
+          const a = itsAlive(1), b = itsAlive(2)
+          const value = itsAlive()
+            .setInput(a,b)
+            .setReducer((a,b)=>a+b)
+
+          value.update()
+
+          expect(value.valueOf()).to.equal(3)
+        })
+
+        it('notifies listeners', () => {
+          const a = itsAlive(1), b = itsAlive(2)
+          const value = itsAlive()
+            .setInput(a,b)
+            .setReducer((a,b)=>a+b)
+
+          const updated = itsAlive(false)
+            .listenTo(value)
+            .setReducer(()=>true)
+
+          value.update()
+
+          expect(updated.valueOf()).to.be.true
+        })
+
+      })
+
+      describe('when reducer result is undefined', () => {
+
+        it('does not update', () => {
+          const a = itsAlive(1), b = itsAlive(2)
+          const value = itsAlive()
+            .setInput(a,b)
+            .setReducer((a,b)=>{
+              return a+b < 10 ?
+                a+b :
+                undefined
+            })
+
+          // show "proper" reducer
+          value.update()
+          expect(value.valueOf()).to.equal(3)
+
+          // "proper" reducer returns undefined
+          a.update(10)
+          value.update()
+          expect(value.valueOf()).to.equal(3)
+        })
+
+        it('does not notify listener', () => {
+          const a = itsAlive(10), b = itsAlive(2)
+          const value = itsAlive()
+            .setInput(a,b)
+            .setReducer((a,b)=>{
+              return a+b < 10 ?
+                a+b :
+                undefined
+            })
+
+          const updated = itsAlive(false)
+            .listenTo(value)
+            .setReducer(()=>true)
+
+          // undefined result does not trigger notification
+          value.update()
+          expect(updated.valueOf()).to.be.false
+
+          // defined result does trigger notifications
+          a.update(1)
+          value.update()
+          expect(updated.valueOf()).to.be.true
+        })
+
+      })
+
+    })
+
+    describe('without proper reducer', () => {
+
+      it('does not update', () => {
+        const a = itsAlive(1), b = itsAlive(2)
+        const value = itsAlive()
+          .setInput(a,b)
+
+        // unset reducer defaults to null
+        value.update()
+        expect(value.valueOf()).to.be.null
+      })
+
+      it('does not notify listener', () => {
+        const a = itsAlive(10), b = itsAlive(2)
+        const value = itsAlive()
+          .setInput(a,b)
+
+        const updated = itsAlive(false)
+          .listenTo(value)
+          .setReducer(()=>true)
+
+        // undefined result does not trigger notification
+        value.update()
+        expect(updated.valueOf()).to.be.false
+      })
+
+    })
+
+  })
+
+})
+
+describe('freezing a value', () => {
+
+  it('sets "_isFrozen" to true', () => {
+    const value = itsAlive(0)
+    value.freeze()
+
+    expect(value._isFrozen).to.be.true
+  })
+
+  it('allows method chaining', () => {
+    const value = itsAlive(0)
+
+    expect(value.freeze()).to.equal(value)
+  })
+
+  it('prevents a value from being updated', () => {
+    const value = itsAlive(0)
+    value.freeze()
+    value.update(1)
+
+    expect(value.valueOf()).to.equal(0)
+  })
+
+  it('update attempts do not notify', () => {
+    const value = itsAlive(0)
+    const updated = itsAlive(false)
+      .listenTo(value)
+      .setReducer(()=>true)
+
+    value.freeze()
+    value.update(1)
+
+    expect(updated.valueOf()).to.false
+  })
+
+  describe('can be unfrozen', () => {
+
+    it('sets "_isFrozen" to false', () => {
+      const value = itsAlive(0)
+
+      value.freeze()
+      expect(value._isFrozen).to.be.true
+
+      value.unfreeze()
+      expect(value._isFrozen).to.be.false
+    })
+
+    it('allows method chaining', () => {
+      const value = itsAlive(0)
+
+      value.freeze()
+      expect(value.unfreeze()).to.equal(value)
+    })
+
+    it('can be updated again', () => {
+      const value = itsAlive(0)
+
+      value.freeze()
+      value.update(1)
+      expect(value.valueOf()).to.equal(0)
+
+      value.unfreeze()
+      value.update(1)
+      expect(value.valueOf()).to.equal(1)
+    })
+
+    it('will notify listeners again', () => {
+      const value = itsAlive(0)
+      const updated = itsAlive(false)
+        .listenTo(value)
+        .setReducer(()=>true)
+
+      value.freeze()
+      value.update(1)
+      expect(updated.valueOf()).to.false
+
+      value.unfreeze()
+      value.update(1)
+      expect(updated.valueOf()).to.true
+    })
+
+  })
+
+})
+
+describe('quieting a value', () => {
+
+  it('sets "_isQuiet" to true', () => {
+    const value = itsAlive(0)
+    value.quiet()
+
+    expect(value._isQuiet).to.be.true
+  })
+
+  it('allows method chaining', () => {
+    const value = itsAlive(0)
+
+    expect(value.quiet()).to.equal(value)
+  })
+
+  it('can still be updated', () => {
+    const value = itsAlive(0)
+    value.quiet()
+    value.update(1)
+
+    expect(value.valueOf()).to.equal(1)
+  })
+
+  it('update attempts do not notify', () => {
+    const value = itsAlive(0)
+    const updated = itsAlive(false)
+      .listenTo(value)
+      .setReducer(()=>true)
+
+    value.quiet()
+    value.update(1)
+
+    expect(updated.valueOf()).to.false
+  })
+
+  describe('can be unquieted', () => {
+
+    it('sets "_isQuiet" to false', () => {
+      const value = itsAlive(0)
+
+      value.quiet()
+      expect(value._isQuiet).to.be.true
+
+      value.unquiet()
+      expect(value._isQuiet).to.be.false
+    })
+
+    it('allows method chaining', () => {
+      const value = itsAlive(0)
+
+      value.quiet()
+      expect(value.unquiet()).to.equal(value)
+    })
+
+    it('can still be updated', () => {
+      const value = itsAlive(0)
+
+      value.quiet()
+      value.update(1)
+      expect(value.valueOf()).to.equal(1)
+
+      value.unquiet()
+      value.update(2)
+      expect(value.valueOf()).to.equal(2)
+    })
+
+    it('will notify listeners again', () => {
+      const value = itsAlive(0)
+      const updated = itsAlive(false)
+        .listenTo(value)
+        .setReducer(()=>true)
+
+      value.quiet()
+      value.update(1)
+      expect(updated.valueOf()).to.false
+
+      value.unquiet()
+      value.update(1)
+      expect(updated.valueOf()).to.true
+    })
+
+  })
 
 })
